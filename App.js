2d13e2f033caebb1167001a0d30786372c1ca178
Rohit Kashyap <rohitdevofficial@gmail.com>
2023-07-27 20:27:34 +0530

    first commit to app

--- App.js
+++ App.js
@@ -2,9 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+ 
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text1}>This is a hello world app from chatGPT app that help me alot</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +19,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text1:{
+    fontSize:200
+  }
 });
+
