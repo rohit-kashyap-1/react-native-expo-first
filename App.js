263f45fd79b3826b0135e51df7a2fdc59d535b95
Rohit Kashyap <rohitdevofficial@gmail.com>
2023-07-27 20:42:22 +0530

    updated app.js

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image,Button,Alert } from 'react-native';
 
 export default function App() {
  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text style={styles.text1}>This is a hello world app from chatGPT app that help me alot</Text>
+      <Image style={styles.image} 
+      source={{
+        uri:'https://i.pinimg.com/originals/f3/7b/52/f37b5273252024d60d9bb5c73bccd627.png'
+      }}
+      />
+      
+      <Text style={styles.text1}>This is our planet</Text>
+      <Text style={{fontSize:32,textAlign:'center',padding:20}}>What do you think about it? does it always look like it was before? No</Text>
+      <Button
+        title="Continue"
+        onPress={() => Alert.alert('Simple Button pressed')}
+       />
       <StatusBar style="auto" />
+      
     </View>
   );
 }
@@ -20,7 +31,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text1:{
-    fontSize:200
+    fontSize:50
+  },
+  image:{
+    width:400,
+    height:400
   }
 });
 
